d5c9c609223f49b0acb7daf6a3b991b36467c463
miso0007 <miso0007@stud.kea.dk>
2025-10-23 12:36:01 +0200

    tilføjet JdbcTemplate og addwishlist metode

--- wishlist/src/main/java/com/misomota/wishlist/repository/WishListRepository.java
+++ wishlist/src/main/java/com/misomota/wishlist/repository/WishListRepository.java
@@ -1,8 +1,49 @@
 package com.misomota.wishlist.repository;
 
+import com.misomota.wishlist.model.WishList;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
 public class WishListRepository {
 
-    public void test(){
-        System.out.println("HELP");
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public WishListRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public WishList addWishList(String wishList) {
+        String sql = "INSERT INTO WishList (wishListName) values (?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(x -> {
+            PreparedStatement preparedStatement = x.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1,wishList);
+            return preparedStatement;
+        }, keyHolder);
+
+        int wishListID = keyHolder.getKey() !=null ? keyHolder.getKey().intValue() : -1;
+
+        if (wishListID != -1) {
+            return new WishList(wishList);
+        } else {
+            throw new RuntimeException("Could not insert wishlist!");
+        }
     }
 }
