e8535b3777c31c374be60bf8766b87f95fd4f2be
monataherzadeh <monataherzadeh05@gmail.com>
2025-10-23 12:11:56 +0200

    GiftWish klasse oprettet

--- wishlist/src/main/java/com/misomota/wishlist/model/GiftWish.java
+++ wishlist/src/main/java/com/misomota/wishlist/model/GiftWish.java
@@ -1,6 +1,17 @@
 package com.misomota.wishlist.model;
 
 public class GiftWish {
-
     private String giftWish;
+
+    public GiftWish(String giftWish) {
+        this.giftWish = giftWish;
+    }
+
+    public String getGiftWish() {
+        return giftWish;
+    }
+
+    public void setGiftWish(String giftWish) {
+        this.giftWish = giftWish;
+    }
 }
