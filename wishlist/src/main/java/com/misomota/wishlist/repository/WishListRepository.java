8ec402ac4f472b2a41dbd07f37aac16fc507f4d9
miso0007 <miso0007@stud.kea.dk>
2025-10-23 11:53:23 +0200

    Repository class added

--- wishlist/src/main/java/com/misomota/wishlist/repository/WishListRepository.java
+++ wishlist/src/main/java/com/misomota/wishlist/repository/WishListRepository.java
@@ -3,6 +3,6 @@ package com.misomota.wishlist.repository;
 public class WishListRepository {
 
     public void test(){
-        System.out.println("Test");
+        System.out.println("HELP");
     }
 }
