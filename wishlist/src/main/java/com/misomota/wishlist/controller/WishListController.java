e715c2526afe512cee63415f6806eec1afde72c8
miso0007 <miso0007@stud.kea.dk>
2025-10-24 14:38:27 +0200

    tilføjet html addWishList og addWish + rettelser i controller

--- wishlist/src/main/java/com/misomota/wishlist/controller/WishListController.java
+++ wishlist/src/main/java/com/misomota/wishlist/controller/WishListController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("Gaveæsken")
+@RequestMapping("/Gaveæsken")
 public class WishListController {
     private final WishListService wishListService;
 
