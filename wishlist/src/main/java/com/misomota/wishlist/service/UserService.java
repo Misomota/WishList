f17de0c9c8dbae44184483071de8b542fd62258f
miso0007 <miso0007@stud.kea.dk>
2025-10-31 10:28:42 +0100

    Tilføjet login og register user

--- wishlist/src/main/java/com/misomota/wishlist/service/UserService.java
+++ wishlist/src/main/java/com/misomota/wishlist/service/UserService.java
@@ -1,4 +1,22 @@
 package com.misomota.wishlist.service;
 
+import com.misomota.wishlist.model.User;
+import com.misomota.wishlist.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.saveUser(user);
+    }
+
+    public boolean validateLogin(String username, String password) {
+        return userRepository.validateLogin(username, password);
+    }
 }
